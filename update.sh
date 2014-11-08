29acf63b6dde9f54d2a3c73d343b849ca3c6da21
Turbo Fredriksson <turbo@bayour.com>
2014-11-08 16:46:19 +0000

    Info on how to create new tags from latest SVN repo.

---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+# Checkout SVN trunc and create upstream GIT tag
+git checkout --orphan svn
+git rm --cached README.md
+rm README.md
+svn checkout svn://svn.code.sf.net/p/scst/svn/trunk .
+git add * .svn
+svn info > info.msg
+git commit -F info.msg
+git tag upstream/`grep ^Revision: info.msg  | sed 's@.*: @@'`
+
+# Delete temporary branch
+git checkout readme
+git branch -D svn
+
+# Checkout debian directory
+git checkout upstream/`grep ^Revision: info.msg  | sed 's@.*: @@'`
+git checkout `git tag -l | grep ^r | tail -n1`
+mv debian/changelog debian/changelog.old
+cat <<EOF > debian/changelog.new
+scst (3.0.0~pre2+svn`grep ^Revision: info.msg  | sed 's@.*: @@'`-ppa1) wheezy; urgency=high
+
+  * Updated to svn`grep ^Revision: info.msg  | sed 's@.*: @@'`
+
+ -- Turbo Fredriksson <turbo@bayour.com>  `date -R`
+
+EOF
+cat debian/changelog.new debian/changelog.old > debian/changelog
+rm debian/changelog.*
+git commit -m "Updated to svn`grep ^Revision: info.msg  | sed 's@.*: @@'`" debian/changelog
+git tag r`grep ^Revision: info.msg  | sed 's@.*: @@'`
+
+# Cleanup
+rm info.msg
