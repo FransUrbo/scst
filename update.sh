b7be5e5dcc06ff9453802fa45e2c4259369f51ee
Turbo Fredriksson <turbo@bayour.com>
2015-03-07 21:51:17 +0100

    Only retreive the revision once.

--- update.sh
+++ update.sh
@@ -10,21 +10,22 @@ svn checkout svn://svn.code.sf.net/p/scst/svn/trunk .
 git add -f * .svn
 svn info > info.msg
 git commit -F info.msg
-git tag upstream/`grep ^Revision: info.msg  | sed 's@.*: @@'`
+REV=`grep ^Revision: info.msg  | sed 's@.*: @@'`
+git tag upstream/$REV
 
 # Delete temporary branch
 git checkout readme
 git branch -D svn
 
 # Checkout debian directory
-git checkout upstream/`grep ^Revision: info.msg  | sed 's@.*: @@'`
+git checkout upstream/$REV
 git checkout `git tag -l | grep ^r | tail -n1` debian
 mv debian/changelog debian/changelog.old
 cat <<EOF > debian/changelog.new
-scst (3.0.0~pre2+svn`grep ^Revision: info.msg  | sed 's@.*: @@'`-ppa1) wheezy; urgency=high
+scst (3.0.0~pre2+svn$REV-ppa1) wheezy; urgency=high
 
   * Include debian directory from `git tag -l | grep ^r | tail -n1`
-    + Updated to svn`grep ^Revision: info.msg  | sed 's@.*: @@'`
+    + Updated to svn$REV
 
  -- Turbo Fredriksson <turbo@bayour.com>  `date -R`
 
@@ -34,11 +35,11 @@ rm debian/changelog.*
 git add debian/changelog
 cat <<EOF > debian/gbp.conf
 [DEFAULT]
-upstream-tag=upstream/`grep ^Revision: info.msg  | sed 's@.*: @@'`
+upstream-tag=upstream/$REV
 EOF
 git add debian/gbp.conf
-git commit -m "Import debian dir + updated to svn`grep ^Revision: info.msg  | sed 's@.*: @@'`"
-git tag r`grep ^Revision: info.msg  | sed 's@.*: @@'`
+git commit -m "Import debian dir + updated to svn$REV"
+git tag r$REV
 
 # Cleanup
 rm info.msg
